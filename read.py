ed297292e117ffef45210c811c6e669128cefbae
Cri690 <cristinazuccali@virgilio.it>
2025-05-22 15:30:04 +0200

    emcee

--- read.py
+++ read.py
@@ -5,7 +5,7 @@ def read():
     # Define the column names
     column_names = ['cc', 'NZ', 'N', 'Z', 'A', 'el', 'o', 'mass', 'unc_mass', 'binding', 'unc_binding', 'B', 'beta', 'unc_beta', 'a', 'atomic_mass', 'unc_atomic_mass']
 
-    df = pd.read_fwf('mass_1.mas20.txt',
+    df = pd.read_fwf('Data.txt',
                     skiprows=36,
                 names=column_names)
     
